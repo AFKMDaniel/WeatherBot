bbbd4587c279b34081fbc6a25332a4c986f6d7f4
AFKMDaniel <danich734@gmail.com>
2022-08-23 21:46:23 +1000

    registering commands on guildCreate

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, GatewayIntentBits, Collection, Routes } = require('discord.js');
+const { REST } = require('@discordjs/rest');
 const fs = require('fs');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
+const commands = [];
+
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.data.name, command);
+	commands.push(command.data.toJSON());
 }
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 client.once('ready', () => {
     console.log('Ready!');
+});
+
+client.on('guildCreate', async (guild) => {
+		try {
+			await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, guild.id), { body: commands });
+			console.log('Success!');
+		}
+		catch (error) {
+			console.error(error);
+		}
 });
 
 client.on('interactionCreate', async interaction => {
